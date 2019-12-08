e8dfef7558823958f2a088df8bf3aafd7ad10d7c
wcgod <wcgod90@gmail.com>
2019-12-09 00:04:50 +0900

    2일차-몽고디비 이용하여 car_list 불러오기

---
+++ day2/sunday02ex10_mongodb_carlist.js
@@ -0,0 +1,125 @@
+// sunday02ex10_mongodb.js
+const http = require('http');
+const express = require('express');
+const app = express();
+const router = express.Router();
+const static = require('serve-static');
+const path = require('path');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
+app.set('port', 3000);
+app.set('views', path.join(__dirname, 'views')); // 접두사
+app.set('view engine', 'ejs'); // 접미사
+
+// __dirname 은 절대 경로
+app.use('/', static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({
+    extendsed: false
+}));
+app.use(bodyParser.json());
+
+// 다른 함수나 다른 기능에서 사용할 수 있도록 전역변수로 선언
+let db;
+
+function connectDB() {
+    let dbUrl = 'mongodb://localhost';
+    MongoClient.connect(dbUrl, (err, client)=>{
+        if(err) throw err;
+
+        db = client.db('vehicle');
+
+        console.log('데이터베이스에 연결되었습니다.', dbUrl);
+    });
+}
+
+let carList = [
+    {
+        _id: 1,
+        name: 'Sonata',
+        price: 2000,
+        company: 'HYUNDAI',
+        year: 2019
+    },
+    {
+        _id: 2,
+        name: 'SM6',
+        price: 3000,
+        company: 'SAMSUNG',
+        year: 2106
+    }
+];
+let seq = 3;
+
+router.route('/car/list').get((req, res) => {
+    //console.log('/car/list 요청됨');
+
+    if(db) {
+        let car = db.collection('car');
+
+        car.find({}).toArray((err, arr)=>{
+            // car_list.ejs 페이지로 렌더링
+            req.app.render('car_list', {
+                carList: arr
+            }, (err2, html) => {
+                if (err2) throw err2;
+
+                res.end(html);
+            });
+        });
+    }
+});
+// : >> params 객체에서 쓸수 있도록 만든다
+router.route('/car/detail/:_id').get((req, res) => {
+    //console.log('/car/detail/:_id 요청됨');
+
+    //params에서 _id를 받아온다.
+    let _id = req.params._id;
+    let car = null;
+    for (item of carList) {
+        if (item._id == _id) {
+            car = item;
+            break;
+        }
+    }
+    if (car == null) {
+        console.log('검색 내용이 없습니다.');
+    }
+
+    //car_detail.ejs 페이지로 렌더링
+    req.app.render('car_detail', {
+        car: car
+    }, (err, html) => {
+        if (err) throw err;
+
+        res.end(html);
+    })
+});
+
+router.route('/car/input').post((req, res) => {
+    //console.log('/car/input 요청됨 ...');
+
+    let car = {
+        _id: seq++,
+        name: req.body.name,
+        price: req.body.price,
+        company: req.body.company,
+        year: req.body.year
+    };
+    // 목록에 넣어준다. 저장하기 - 나중에 몽고디비에 저장
+    carList.push(car);
+
+    // 목록으로 돌아간다.
+    res.redirect('/car/list');
+
+});
+
+//router 미들웨어 등록 (위치는 서버 실행하기 바로 위에 하는게 가장 안정적)
+app.use('/', router);
+
+const server = http.createServer(app); // http와 express 결합
+
+server.listen(app.get('port'), () => {
+    console.log(`http://localhost:${app.get('port')}`);
+    connectDB();
+});
